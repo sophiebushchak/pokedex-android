04c59d42467e234f6ea9865ce934c5fea59729c9
Sophie Bushchak <sophie.bushchak@hva.nl>
2020-12-30 13:55:02 +0100

    Change app bar and improve pokedex card layout

--- app/src/main/java/com/example/pokedata/rest/PokeApiConfig.kt
+++ app/src/main/java/com/example/pokedata/rest/PokeApiConfig.kt
@@ -2,7 +2,7 @@ package com.example.pokedata.rest
 
 class PokeApiConfig {
     companion object {
-        const val HOST = "http://192.168.43.206:8080/"
+        const val HOST = "http://62.194.247.191:8080/"
         const val BASE_URL = "api/"
     }
 }
--- app/src/main/java/com/example/pokedata/ui/MainActivity.kt
+++ app/src/main/java/com/example/pokedata/ui/MainActivity.kt
@@ -5,28 +5,28 @@ import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import com.example.pokedata.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(findViewById(R.id.toolbar))
+        setSupportActionBar(findViewById(R.id.topAppBar))
+        topAppBar.setOnMenuItemClickListener{ menuItem ->
+            when (menuItem.itemId) {
+                R.id.action_search -> {
+                    println("Clicked search!")
+                    true
+                }
+                else -> false
+            }
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
     }
 }
